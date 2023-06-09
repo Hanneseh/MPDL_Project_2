3b3971cc0d336fb95a26aeaa45eaca16bacee68a
DamyGit <105376200+DamyGit@users.noreply.github.com>
2023-06-09 23:23:54 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -8,23 +8,24 @@ import torch
 if __name__== "__main__":
         
     # Set device
-    device = "cpu"
-    if torch.backends.mps.is_available():
-        device = "mps"
-    elif torch.cuda_is_available():
-        device = "cuda"
+    device = "cuda:5"
+    #if torch.backends.mps.is_available():
+    #    device = "mps"
+    #elif torch.cuda_is_available():
+    #    device = "cuda"
 
     device = torch.device(device)
 
     #Set Paths
-    PATH_TRAIN = "/Users/pauladler/MPDL_Project_2/data/train"
-    PATH_VAL = "/Users/pauladler/MPDL_Project_2/data/val"
-    PATH_TEST = "/Users/pauladler/MPDL_Project_2/data/test"
-    PATH_SAVE = "nix.pth"
+    PATH_TRAIN = "/raid/USERDATA/ganndacw/MPDL/datasets/laion_subset_10k_splits/train"
+    PATH_VAL = "/raid/USERDATA/ganndacw/MPDL/datasets/laion_subset_10k_splits/val"
+    PATH_TEST = "/raid/USERDATA/ganndacw/MPDL/datasets/laion_subset_10k_splits/test"
+    PATH_SAVE = "nix_training0906.pth"
 
     #Set Parameters for creating the Dataset
     num_workers = 5
-    batch_size = 8
+    batch_size = 16
+    num_epochs = 200
 
     # Set Parameters for model
     img_width, img_height = 512, 512
@@ -40,12 +41,12 @@ if __name__== "__main__":
 
     model = NIX(img_width, img_height)
     model = model.to(device)
-    model = train_model(model, train_dataloader, val_dataloader, learning_rate, device)
+    model = train_model(model, train_dataloader, val_dataloader, learning_rate, device, num_epochs)
 
     torch.save(model.state_dict(), PATH_SAVE)
 
     test_data = data(PATH_TEST)
     test_data = DataLoader(test_data, batch_size=batch_size, num_workers=num_workers, shuffle=True)
 
-    test_loss = val_epoch(model, test_data, device)
-    print("[INFO] Val loss: {:.6f}".format(test_loss))
+    test_loss, IoU = val_epoch(model, test_data, device)
+    print("[INFO] Test: Val loss: {:.6f}, IoU: {}".format(test_loss, IoU))
